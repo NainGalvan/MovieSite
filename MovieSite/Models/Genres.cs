4b6dac8c057c7641609f147a99b08efa8028d1db
ngalvan <naingalvan12@gmail.com>
2022-08-20 19:58:42 -0700

    Uploading first instance of MovieSite

---
+++ MovieSite/Models/Genres.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieSite.Models
+{
+    public class Genre
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Genres
+    {
+        public Genre[] genres { get; set; }
+    }
+}
---
+++ MovieSite/MovieSite/Models/PopularMovies.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieSite.Models
+{
+    public class PopularMovies
+    {
+    }
+}
